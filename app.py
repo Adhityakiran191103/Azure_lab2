a45d2349e4bd71529fc68481879cf6fa2d496a34
Adhi_Azure <adhityakiran10@gmail.com>
2026-01-16 15:17:45 +0100

    Azure Lab 2 - all parts

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def integrate(lower, upper, N):
     return f"{result:.6f}"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=8000)
--- locustfile.py
+++ locustfile.py
@@ -1,8 +1,19 @@
 from locust import HttpUser, task, between
 
-class IntegrationUser(HttpUser):
-    wait_time = between(1, 3)  # Wait 1-3s between tasks
+class NumericalUser(HttpUser):
+    wait_time = between(0.1, 0.5)
+
+    # Function App host (no /api, no params)
+    host = "https://adhitya-numint-fn-g0dja9cuahhkdkeu.switzerlandnorth-01.azurewebsites.net"
 
     @task
-    def compute_integral(self):
-        self.client.get("/numerical_integral/0/3.14159/100000")  # Heavy N for load
+    def call_function(self):
+        self.client.get(
+            "/api/numerical_integral_fn",
+            params={
+                "code": "<your-function-key>",   # from Get Function URL
+                "lower": 0.0,
+                "upper": 3.14159,
+                "N": 1000                     # big enough to keep CPU busy
+            }
+        )
